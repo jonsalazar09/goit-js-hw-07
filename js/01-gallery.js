38671af2bffc50bcac9e810e00a294aa7c0f28c3
jonsalazar09 <jonsalazar09@gmail.com>
2024-05-03 01:37:19 +0800

    hw7

---
+++ js/01-gallery.js
@@ -0,0 +1,63 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+const imageGallery = document.querySelector('.gallery');
+const cardsMarkup = createImageCardsMarkup(galleryItems);
+
+function createImageCardsMarkup(images) {
+  return images
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>`
+    )
+    .join('');
+}
+
+imageGallery.insertAdjacentHTML('beforeend', cardsMarkup);
+
+imageGallery.addEventListener('click', onCurrentImageClick);
+
+function onCurrentImageClick(evt) {
+  evt.preventDefault();
+
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  const currentImageLink = evt.target.dataset.source;
+  const marcup = `<img src="${currentImageLink}">`;
+
+  const instance = basicLightbox.create(marcup, {
+    onShow: () => onOpenInstance(),
+    onClose: () => onCloseInstance(),
+  });
+
+  instance.show();
+
+  function onOpenInstance() {
+    window.addEventListener('keydown', onEscKeyPress);
+  }
+
+  function onCloseInstance() {
+    window.removeEventListener('keydown', onEscKeyPress);
+  }
+
+  function onEscKeyPress(evt) {
+    const ESC_KEY_CODE = 'Escape';
+    const isEscKey = evt.code === ESC_KEY_CODE;
+
+    if (isEscKey) {
+      instance.close();
+    }
+  }
+}
+
+console.log(galleryItems);
